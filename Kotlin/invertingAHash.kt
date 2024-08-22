64b8fe2c6771cfc7bc1a98837a7ca5fe17a6d376
Courtlyn Boykin <courtlynb@google.com>
2024-08-22 10:31:35 -0400

    optimized inverting a hash in kt

--- Kotlin/invertingAHash.kt
+++ Kotlin/invertingAHash.kt
@@ -10,3 +10,13 @@ fun invertMap(hash: Map<Any, Any>): Map<Any, Any> {
      val map: Map<Any, Any> = mapOf('a' to 1, 'b' to 2, 'c' to 3)
      println(invertMap(map))
  }
+
+// 8.22
+fun invertMap(hash: Map<Any, Any>): Map<Any, Any> {
+    var newMap = mutableMapOf<Any, Any>()
+    
+    for((k, v) in hash) {
+        newMap.put(v, k)
+    }
+    return newMap
+}
