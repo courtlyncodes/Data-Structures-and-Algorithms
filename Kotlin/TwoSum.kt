3c4785890ec393e3e8be11a49d2ab66c2f5327c1
Courtlyn Boykin <courtlynb@google.com>
2024-08-26 11:25:00 -0400

    optimized Two Sum in Kotlin

--- Kotlin/TwoSum.kt
+++ Kotlin/TwoSum.kt
@@ -21,3 +21,28 @@ class Solution {
         return arr.toIntArray()
     }
 }
+
+//8.26
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val arr = mutableListOf<Int>()
+        val map = mutableMapOf<Int, Int>()
+
+        for(i in nums.indices) {
+            map.set(nums[i], i)
+        }
+
+        for(i in nums.indices) {
+            val complement = target - nums[i]
+
+            if(map.containsKey(complement) && i != map.get(complement)) {
+                    arr.add(i)
+                    arr.add(map.getValue(complement))
+                    return arr.toIntArray()
+            }
+                    
+                } 
+                 return arr.toIntArray()
+            }
+        }
+      
