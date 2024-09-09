40d77498c72a2ee6da7f2faca99b02f46e9d3a18
Courtlyn Boykin <courtlynb@google.com>
2024-09-09 11:11:59 -0400

    split Kotlin folder

---
+++ Kotlin/Leetcode/ImplementQueueUsingStacks.kt
@@ -0,0 +1,30 @@
+// 9.9
+class MyQueue() {
+
+    private var popping = ArrayDeque<Int>()
+    private var pushing = ArrayDeque<Int>()
+
+    fun push(x: Int) {
+        pushing.add(x)
+    }
+
+    fun pop(): Int {
+        if(popping.isEmpty()) {
+            while(pushing.isNotEmpty()) {
+                popping.add(pushing.removeLast())
+            }
+        }
+        return popping.removeLast()
+    }
+
+    fun peek(): Int {
+         if(popping.isEmpty() ) {
+            while(pushing.isNotEmpty()) {
+                popping.add(pushing.removeLast())
+            }
+        }
+        return popping.last()
+    }
+
+    fun empty(): Boolean = if(popping.isEmpty() && pushing.isEmpty()) true else false
+}
