9720e826f656e86537db2c6b2cf11a50606db158
Courtlyn Boykin <courtlynb@google.com>
2024-09-10 10:46:00 -0400

    redo Implement queue using stacks kt

--- Kotlin/Leetcode/ImplementQueueUsingStacks.kt
+++ Kotlin/Leetcode/ImplementQueueUsingStacks.kt
@@ -28,3 +28,35 @@ class MyQueue() {
 
     fun empty(): Boolean = if(popping.isEmpty() && pushing.isEmpty()) true else false
 }
+
+//9.10
+class MyQueue() {
+
+    private var pushing = ArrayDeque<Int>()
+    private var popping = ArrayDeque<Int>()
+
+    fun push(x: Int) {
+        pushing.add(x)
+    }
+
+    fun pop(): Int {
+        transfer()
+        return popping.removeLast()
+    }
+
+    fun peek(): Int {
+        transfer()
+        return popping.last()
+    }
+
+    fun empty(): Boolean = popping.isEmpty() && pushing.isEmpty()
+
+    private fun transfer() {
+              if(popping.isEmpty()){
+            while(pushing.isNotEmpty()){
+                popping.add(pushing.removeLast())
+            }
+        }
+    }
+}
+
