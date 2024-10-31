1b3ab53242541623f8160f742c9cfe2221092cac
Courtlyn Boykin <courtlynb@google.com>
2024-10-31 16:07:58 -0400

    complete linked list cycle with hash set

--- Kotlin/Leetcode/Grind75/LinkedListCycle.kt
+++ Kotlin/Leetcode/Grind75/LinkedListCycle.kt
@@ -18,3 +18,31 @@ class Solution {
         return false
     }
 }
+
+// Hash Set implementation
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+ class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+      var set = mutableSetOf<ListNode?>()
+      var curr = head
+
+      while(curr != null) {
+        if(!set.contains(curr)) {
+            set.add(curr)
+            curr = curr.next
+        } else {
+            return true
+        }
+      } 
+      return false
+    }
+}
