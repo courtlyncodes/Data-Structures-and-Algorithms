49f8c934fd2bc132886f2e9dde913118b3909ccd
Courtlyn Boykin <courtlynb@google.com>
2024-12-11 11:15:40 -0500

    optimize fibonacci sum last digit for larger n

--- Kotlin/Algorithmic Toolbox/FibonacciSumLastDigit.kt
+++ Kotlin/Algorithmic Toolbox/FibonacciSumLastDigit.kt
@@ -1,14 +1,18 @@
 import java.util.*
 
-fun getFibonacciSumNaive(n: Long): Long {
-    if (n <= 1)
-        return n
 
+fun getFibonacciSumNaive(n: Long): Long {
+    if (n <= 1) return n
+
+    val reduced = n % 60L
+
+    if(reduced <= 1) return reduced
+    
     var previous: Long = 0
     var current: Long = 1
     var sum: Long = 1
 
-    for (i in 0 until n - 1) {
+    for (i in 0 until reduced) {
         val tmpPrevious = previous
         previous = current
        	current = (current + tmpPrevious) % 10  
@@ -17,6 +21,7 @@ fun getFibonacciSumNaive(n: Long): Long {
 
     return sum % 10
 }
+
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val n = scanner.nextLong()
