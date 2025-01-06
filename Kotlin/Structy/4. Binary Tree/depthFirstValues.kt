2a4cb833024d93fefefb275fcede9b3934544a40
Courtlyn Boykin <courtlynb@google.com>
2025-01-06 13:10:20 -0500

    add depth first search on binary tree iteratively

---
+++ Kotlin/Structy/4. Binary Tree/depthFirstValues.kt
@@ -0,0 +1,66 @@
+// Write a function, depthFirstValues, that takes in the root of a binary tree. 
+// The function should return an array containing all values of the tree in depth-first order.
+
+// Iterative approach
+class Node(val value: Char) {
+    var left: Node? = null
+    var right: Node? = null
+}
+
+fun depthFirstValues(root: Node?): CharArray {
+    var arr = mutableListOf<Char>()
+    var stack = ArrayDeque<Node>()
+   
+    
+    if(root == null) return arr.toCharArray()
+    
+    stack.add(root)
+    
+   while(stack.isNotEmpty()) {
+   		var curr = stack.removeLast()
+        arr.add(curr.value)
+        
+        curr.right?.let { stack.add(it) }
+       	curr.left?.let { stack.add(it) } 
+           
+      
+   }
+    
+    return arr.toCharArray()
+        
+}
+
+
+fun main() {
+    // Test 1
+    val a = Node('a')
+    val b = Node('b')
+    val c = Node('c')
+    val d = Node('d')
+    val e = Node('e')
+    val f = Node('f')
+
+    a.left = b 
+    a.right = c
+    b.left = d
+    b.right = e 
+    c.left = f    
+    
+    println(depthFirstValues(a))
+    
+    // Test 2
+    println(depthFirstValues(null))
+    
+    // Test 3
+    val g = Node('g')
+    val h = Node('h')
+    val j = Node('j')
+    
+    println(depthFirstValues(g))
+    
+  	// Test 4
+    val k = Node('k')
+    
+    println(depthFirstValues(k))
+
+}
