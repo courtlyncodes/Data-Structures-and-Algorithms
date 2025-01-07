2842a8f41b6835a479b33f492d9d48f6ec88c8e1
Courtlyn Boykin <courtlynb@google.com>
2025-01-07 10:32:12 -0500

    tree includes structy

---
+++ Kotlin/Structy/4. Binary Tree/treeIncludes.kt
@@ -0,0 +1,60 @@
+// Write a function, treeIncludes, that takes in the root of a binary tree and a target value. 
+// The function should return a boolean indicating whether or not the value is contained in the tree.
+
+class Node(val value: Any) {
+    var left: Node? = null
+    var right: Node? = null
+}
+
+fun treeIncludes(root: Node?, target: Any): Boolean {
+    if (root == null) return false
+    
+    var stack = ArrayDeque<Node>()
+    stack.add(root)
+    
+    while(stack.isNotEmpty()) {
+        var curr = stack.removeLast()
+        
+        if(curr.value == target) return true
+          
+        curr.right?.let { stack.add(it) }
+        curr.left?.let { stack.add(it) }
+    }
+    return false
+}
+
+
+
+fun main() {
+    // Test 1
+    val a = Node('a')
+    val b = Node('b')
+    val c = Node('c')
+    val d = Node('d')
+    val e = Node('e')
+    val f = Node('f')
+
+    a.left = b 
+    a.right = c
+    b.left = d
+    b.right = e 
+    c.left = f    
+    
+    println(treeIncludes(a, 'e'))
+    
+    // Test 2   
+    println(treeIncludes(a, 'a'))
+    
+    // Test 3
+  	println(treeIncludes(a, 'n'))
+    
+    // Test 4
+  	println(treeIncludes(a, 'f'))
+    
+    // Test 5
+  	println(treeIncludes(a, 'p'))
+    
+    // Test 6
+  	println(treeIncludes(null, 'b'))
+    
+}
